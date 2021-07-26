ccff5a6ee1cb302603a2d0675c4ce5590b882e90
humfltay <humfltay@uw.edu>
2021-07-26 13:36:11 -0700

    SQL File support addition

---
+++ TriviaMaze360/src/Model/Maze.java
@@ -0,0 +1,17 @@
+/*TCSS 360 Maze Project*/
+package Model;
+
+/**
+ * @author Taylor Humfleet
+ * @author Mason Hanson
+ * @author Cordell Hampshire
+ * 
+ *
+ */
+public class Maze {
+    //blank constructor
+    private Maze() {
+        
+    }
+    
+}
