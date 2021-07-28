ad86229298f29c8c956128ae1c73a7c96172135e
humfltay <humfltay@uw.edu>
2021-07-27 23:44:06 -0700

    Merge of Cordels Branch with some refactoring

---
+++ TriviaMaze360/src/save/MazeGameState.java
@@ -0,0 +1,14 @@
+package save;
+
+import java.io.Serializable;
+
+import model.Maze;
+import model.User;
+
+public interface MazeGameState extends Serializable {
+    //User theUser;
+    
+    
+    
+    
+}
